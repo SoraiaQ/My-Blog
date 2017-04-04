0c156253195d7811e967922e7664b0c032c0c7bc
SoraiaQ <soraia.qpires@gmail.com>
2017-04-04 15:23:33 +0100

    Add Visual studio commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number");
+            int x = int.Parse(Console.ReadLine());
+            if (isDivisibleBy(x, 2))
+            {
+                Console.WriteLine("That's even!");
+            } else
+            {
+                Console.WriteLine("That's odd");
+            }
+
+
+        }
+        
+
+        static bool isDivisibleBy(int x, int y)
+        {
+            int remainder = x % y;
+            return remainder == 0;
+        }
+
+    
+    }
+}
